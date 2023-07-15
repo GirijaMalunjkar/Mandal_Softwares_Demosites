eaf71d21f7471ca3a952011f7d01ec4b0f642c23
Girija Malunjkar <girijamalunjkar666@gmail.com>
2023-07-15 18:14:53 +0530

    Design Contact page.

--- rk/src/pages/Contact.js
+++ rk/src/pages/Contact.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-import "../assets/css/contact.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import '../assets/css/contact.css'
 
 function ContactPage() {
-  const [darkMode, setDarkMode] = useState(false);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
 
-   const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({
     name: '',
     phone: '',
     email: '',
@@ -19,8 +20,10 @@ function ContactPage() {
     city: '',
     state: '',
     pinCode: '',
-     gender: '',
+    gender: '',
   });
+
+  const [gender, setGender] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -32,177 +35,163 @@ function ContactPage() {
     console.log(formData);
   };
 
+  const handleGenderChange = (e) => {
+    setGender(e.target.value);
+  };
+
+
   return (
-    <div className={`contact-container`}>
-
-      <div className="left-col">
-        <img className='site-logo' src={require("../assets/images/logos/Rangkatha1.webp")} alt='Rangkatha1.webp' />
-      </div>
-      <div className="right-col">
-      <div className="bg-image"></div>
-
-      <div className="inner">
-          <h1>Registration</h1>
-          <div className="border"></div>
-          </div>
-          <form onSubmit={handleSubmit}>
-          <div className="formbold-mb-5">
-            <label htmlFor="name" className="formbold-form-label">
-              Full Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Full Name"
-              className="formbold-form-input"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="formbold-mb-5">
-            <label htmlFor="phone" className="formbold-form-label">
-              Phone Number
-            </label>
-            <input
-              type="text"
-              name="phone"
-              id="phone"
-              placeholder="Enter your phone number"
-              className="formbold-form-input"
-              value={formData.phone}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="formbold-mb-5">
-            <label htmlFor="email" className="formbold-form-label">
-              Email Address
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter your email"
-              className="formbold-form-input"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="formbold-mb-5">
-            <label className="formbold-form-label">Gender</label>
-            <div className="formbold-form-input">
-              <label>
-                <input
-                  type="radio"
-                  name="gender"
-                  value="male"
-                  checked={formData.gender === 'male'}
-                  onChange={handleChange}
-                />{' '}
-                Male
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  name="gender"
-                  value="female"
-                  checked={formData.gender === 'female'}
-                  onChange={handleChange}
-                />{' '}
-                Female
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  name="gender"
-                  value="other"
-                  checked={formData.gender === 'other'}
-                  onChange={handleChange}
-                />{' '}
-                Other
-              </label>
-            </div>
-          </div>
-
-
-          <div className="formbold-mb-5 formbold-pt-3">
-            <label className="formbold-form-label formbold-form-label-2">
-              Address Details
-            </label>
-            <div className="flex flex-wrap formbold--mx-3">
-              <div className="w-full sm:w-half formbold-px-3">
-                <div className="formbold-mb-5">
-                  <input
-                    type="text"
-                    name="area"
-                    id="area"
-                    placeholder="Enter area"
-                    className="formbold-form-input"
-                    value={formData.area}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
-              <div className="w-full sm:w-half formbold-px-3">
-                <div className="formbold-mb-5">
-                  <input
-                    type="text"
-                    name="city"
-                    id="city"
-                    placeholder="Enter city"
-                    className="formbold-form-input"
-                    value={formData.city}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
-              <div className="w-full sm:w-half formbold-px-3">
-                <div className="formbold-mb-5">
-                  <input
-                    type="text"
-                    name="state"
-                    id="state"
-                    placeholder="Enter state"
-                    className="formbold-form-input"
-                    value={formData.state}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
-              <div className="w-full sm:w-half formbold-px-3">
-                <div className="formbold-mb-5">
-                  <input
-                    type="text"
-                    name="pinCode"
-                    id="post-code"
-                    placeholder="Post Code"
-                    className="formbold-form-input"
-                    value={formData.pinCode}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <button type="submit" className="formbold-btn">
-              Register
-            </button>
-          </div>
-        </form>
-      
-        <div id="error">{errorMessage}</div>
-        <div id="success-msg">{successMessage}</div>
+    <div className="c_container">
+      <div className="card">
+        <div className="left">
+          <img src={require("../assets/images/Wedding/wedding21.webp")} alt="Background" />
         </div>
-       
-        
+        <div className="right-col">
+          <div className="bg-image"></div>
 
+          <div className="inner" style={{ zIndex: 12, position: 'relative' }}>
+            <h1>Registration</h1>
+          </div>
+          <form onSubmit={handleSubmit} style={{ zIndex: 12, position: 'relative' }}>
+            <div className="formbold-mb-5">
+              <label htmlFor="name" className="formbold-form-label">
+                Full Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Full Name"
+                className="formbold-form-input"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formbold-mb-5">
+              <label htmlFor="phone" className="formbold-form-label">
+                Phone Number
+              </label>
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                placeholder="Enter your phone number"
+                className="formbold-form-input"
+                value={formData.phone}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formbold-mb-5">
+              <label htmlFor="email" className="formbold-form-label">
+                Email Address
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+                className="formbold-form-input"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formbold-mb-5">
+            <label htmlFor="gender" className="formbold-form-label">
+            <p>Select your gender:</p>
+              </label>
+              <label htmlFor="male" className="formbold-form-label">
+                    <input type="radio" value="male" checked={gender === 'male'} onChange={handleGenderChange} />
+                    Male
+                  </label>
+                  <label htmlFor="female" className="formbold-form-label">
+                    <input type="radio" value="female" checked={gender === 'female'} onChange={handleGenderChange} />
+                    Female
+                  </label>
+              {/* <input
+                type="gender"
+                name="email"
+                id="email"
+                placeholder="gender"
+                className="formbold-form-input"
+                value={formData.email}
+                onChange={handleChange}
+              /> */}
+            </div>
 
-      
+            <div className="formbold-mb-5 formbold-pt-3">
+              <label className="formbold-form-label formbold-form-label-2">
+                Address Details
+              </label>
+              <div className="flex flex-wrap formbold--mx-3">
+                <div className="w-full sm:w-half formbold-px-3">
+                  <div className="formbold-mb-5">
+                    <input
+                      type="text"
+                      name="area"
+                      id="area"
+                      placeholder="Enter area"
+                      className="formbold-form-input"
+                      value={formData.area}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="w-full sm:w-half formbold-px-3">
+                  <div className="formbold-mb-5">
+                    <input
+                      type="text"
+                      name="city"
+                      id="city"
+                      placeholder="Enter city"
+                      className="formbold-form-input"
+                      value={formData.city}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="w-full sm:w-half formbold-px-3">
+                  <div className="formbold-mb-5">
+                    <input
+                      type="text"
+                      name="state"
+                      id="state"
+                      placeholder="Enter state"
+                      className="formbold-form-input"
+                      value={formData.state}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="w-full sm:w-half formbold-px-3">
+                  <div className="formbold-mb-5">
+                    <input
+                      type="text"
+                      name="pinCode"
+                      id="post-code"
+                      placeholder="Post Code"
+                      className="formbold-form-input"
+                      value={formData.pinCode}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+            <div className="usersubmit">
+          <input type="submit" value="Register" />
+        </div>
+            </div>
+          </form>
+
+          <div id="error">{errorMessage}</div>
+          <div id="success-msg">{successMessage}</div>
+        </div>
+
+      </div>
     </div>
   );
-}
+};
 
 export default ContactPage;
